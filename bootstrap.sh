88118f8481e3925b408df136ba3bb336d018a658
Lucas Cavalcante <lucasmcavalcante@gmail.com>
2020-02-04 18:17:45 -0300

    zsh

--- bootstrap.sh
+++ bootstrap.sh
@@ -38,16 +38,13 @@ function setup_home () {
     cd $HOME
     #rm -rf $HOME/.vim
     rm -rf $HOME/.vimrc
+    rm -rf $HOME/.tmux.conf
 	tic -o $HOME/.terminfo $HOME/.dotfiles/xterm-256color.terminfo
 	#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 	ln -s $HOME/.dotfiles/vimrc $HOME/.vimrc || true
     ln -s $HOME/.dotfiles/tmux.conf $HOME/.tmux.conf || true
-    #rm -f $HOME/.gitconfig && ln -s $HOME/.dotfiles/gitconfig $HOME/.gitconfig || true
-    #ln -s $HOME/.dotfiles/gitignore_global $HOME/.gitignore_global || true
-    #ln -s $HOME/.dotfiles/NERDTreeBookmarks $HOME/.NERDTreeBookmarks || true
-    #mkdir -p $HOME/.ssh && ln -s $HOME/.dotfiles/sshconf  $HOME/.ssh/config || true
-    #rm -f $HOME/.bashrc && ln -s $HOME/.dotfiles/bashrc $HOME/.bashrc || true
-    #rm -f $HOME/.bash_profile && ln -s $HOME/.dotfiles/bashrc $HOME/.bash_profile || true
+	apt install -y zsh 
+	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" | echo "Y"
 
 }
 
